ae5767fb992f19009744ea2c8dde648426e6bd62
Lukasz Kipka <lukasz.kipka@pb.com>
2023-05-30 21:52:48 +0200

    Sort todos also by creation time (newest first)

--- HabiticaAPI/Models/Todos/TodoComparer.cs
+++ HabiticaAPI/Models/Todos/TodoComparer.cs
@@ -16,7 +16,7 @@ public class TodoComparer : IComparer<Todo>
     public int Compare(Todo? x, Todo? y)
     {
         const int lowestPriority = 100;
-        
+
         if (x == null)
         {
             if (y == null)
@@ -26,15 +26,21 @@ public class TodoComparer : IComparer<Todo>
 
         if (y == null)
             return 1;
-        
-        Tag? xTagForSorting, yTagForSorting;
-        int xTagPriority, yTagPriority;
 
-        xTagForSorting = x.Tags.FirstOrDefault(tag => _tagsOrder.Values.Any(orderedTag => orderedTag.Name == tag.Name));
-        xTagPriority = xTagForSorting is null ? lowestPriority : _tagsOrder.Where(t => t.Value.Name == xTagForSorting?.Name).Select(t => t.Key).FirstOrDefault();
+        var xTagForSorting = x.Tags.FirstOrDefault(tag => _tagsOrder.Values.Any(orderedTag => orderedTag.Name == tag.Name));
+        var xTagPriority = xTagForSorting is null ? lowestPriority : _tagsOrder.Where(t => t.Value.Name == xTagForSorting?.Name).Select(t => t.Key).FirstOrDefault();
 
-        yTagForSorting = y.Tags.FirstOrDefault(tag => _tagsOrder.Values.Any(orderedTag => orderedTag.Name == tag.Name));
-        yTagPriority = yTagForSorting is null ? lowestPriority : _tagsOrder.Where(t => t.Value.Name == yTagForSorting?.Name).Select(t => t.Key).FirstOrDefault();
+        var yTagForSorting = y.Tags.FirstOrDefault(tag => _tagsOrder.Values.Any(orderedTag => orderedTag.Name == tag.Name));
+        var yTagPriority = yTagForSorting is null ? lowestPriority : _tagsOrder.Where(t => t.Value.Name == yTagForSorting?.Name).Select(t => t.Key).FirstOrDefault();
+
+        if (xTagPriority == yTagPriority)
+        {
+            // Assign higher priority to newest todos
+            if (x.CreationTime > y.CreationTime)
+                xTagPriority = -1;
+            else
+                yTagPriority = -1;
+        }
 
         _logger.LogDebug("\rx: {xTodoName}\n \t{xTagNames}\n y: {yTodoName}\n \t{yTagNames}", x.TaskName, x.ListTagNames(), y.TaskName, y.ListTagNames());
         _logger.LogDebug(xTagPriority.CompareTo(yTagPriority).ToString() + Environment.NewLine);
